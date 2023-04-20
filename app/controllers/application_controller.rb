82fe7e4b32709868500ae81a43d4beb511b3d0bb
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-04-20 11:50:54 +0000

    ヘッダー・フッター作成中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    about_path
+    public_about_path
   end
 
   protected
   
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   def configure_permitted_parameters
