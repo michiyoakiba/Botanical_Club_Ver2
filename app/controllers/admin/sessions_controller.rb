1f9de082737ba954e39b462fe3ade5cc673f20ae
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-04-23 05:59:01 +0000

    ログイン(管理者側)完成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -9,20 +9,25 @@ class Admin::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    admin = Admin.find_by(email: params[:admin][:email])
+    if admin.valid_password?(params[:admin][:password])
+      sign_in admin
+      redirect_to admin_customers_path
+    end
+    # super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!, except: [:top, :about]
+  #before_action :authenticate_customer!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    public_plants_path
+    if resource.is_a?(Admin)
+      admin_customers_path
+    else
+      public_plants_path
+    end
   end
-
+  
   def after_sign_out_path_for(resource)
-    root_path
+    pp resource
+    if resource == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
   end
   
   protected
