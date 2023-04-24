77ed2ed775e2fb6148bb713c4a8ea78354000616
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-04-24 05:37:08 +0000

    plants_controller修正

--- db/migrate/20230423023429_remove_name_from_plants.rb
+++ db/migrate/20230423023429_remove_name_from_plants.rb
@@ -1,5 +1,6 @@
 class RemoveNameFromPlants < ActiveRecord::Migration[6.1]
   def change
     remove_column :plants, :user_id, :integer
+    
   end
 end
--- db/migrate/20230424033910_add_name_to_plants.rb
+++ db/migrate/20230424033910_add_name_to_plants.rb
@@ -1,5 +1,6 @@
 class AddNameToPlants < ActiveRecord::Migration[6.1]
   def change
     add_column :plants, :customer_id, :integer
+    add_column :plants, :genre_id, :integer
   end
 end
