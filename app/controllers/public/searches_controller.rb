493e8001dc1ca7a33e3e01be3615441069ba58f9
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-04-27 15:49:21 +0000

    検索機能ページネーション追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,7 +5,7 @@ class Public::SearchesController < ApplicationController
 		@content = params[:content]
 		@method = params[:method]
 		if @model == 'customer'
-			@records = Customer.search_for(@content, @method)
+			@records = Customer.search_for(@content, @method).page(params[:page]).per(5)
 		else
 			@records = Plant.search_for(@content, @method).page(params[:page]).per(3)
 		end
