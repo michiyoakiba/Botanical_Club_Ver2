0d4a591d7fc99a76304ea5d87f5da63e3ccef692
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-04-30 12:13:15 +0000

    管理者側レイアウト修正

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,9 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def self.find_for_database_authentication(warden_conditions)
+    email = warden_conditions[:email].to_s.downcase.strip
+    find_by(email: email, organization_id: warden_conditions[:organization_id])
+  end
 end
